7bb26bd8017f645d8370e029a7969ee02b5921d3
CJF <2779493524@qq.com>
2024-11-08 12:48:08 +0800

    6666666666666

---
+++ movie/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Movie,Review
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
+from .forms import ReviewForm
+
+# Create your views here.
+def moviehome(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movie_list = Movie.objects.filter(title__contains=searchTerm)
+    else:
+        movie_list = Movie.objects.all()
+    paginator = Paginator(movie_list, 2)
+    page_number = request.GET.get('page', 1)
+    movies = paginator.page(page_number)
+    # movies = Movie.objects.all()
+    return render(request, 'moviehome.html',
+                  {'searchTerm':searchTerm, 'movies':movies})
+
+def home(request):
+    return render(request,'home.html', {'name':'CJF','class':'22bigdata2'})
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
+
+def moviedetail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'moviedetail.html', {'movie': movie})
+
+def createmoviereview(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'GET' :
+        return render(request, 'createmoviereview.html' ,
+        {'form':ReviewForm , 'movie':movie})
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.movie = movie
+            newReview.save()
+            return redirect('moviedetail',newReview.movie.id)
+        except ValueError:
+            return render(request,'createmoviereview.html', {'form':ReviewForm, 'error':'非法数据'})
